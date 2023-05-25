9dd2dd019ac5dfcf7dbd3ab1756831a0d407905f
hzein <hzein286@gmail.com>
2023-05-25 14:36:00 -0700

    Upload App

---
+++ backend/routes/workouts.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
+
+const router = express.Router();
+
+//require auth for all workout routes
+router.use(requireAuth);
+
+//GET All workouts
+router.get("/", getWorkouts);
+
+//GET single workout
+router.get("/:id", getWorkout);
+
+//POST a new workout
+router.post("/", createWorkout);
+
+//DELETE a workout
+router.delete("/:id", deleteWorkout);
+
+//UPDATE a workout
+router.patch("/:id", updateWorkout);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
+
+// Express App
+const app = express();
+
+//middleware
+//This give us access to the json body in routes
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
+
+//Connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
